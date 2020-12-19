40dbe95c5c83b02b672cba4e3f5e829ae95ee293
Aadhitya A <59508546+echo-864@users.noreply.github.com>
2020-12-19 17:12:20 +0530

    Modify setup

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
   name='codeit',
   version='0.0.2',
   author='Aadhitya A',
+  description = 'A CLI tool to create CPP files',
+  long_description = README,
+  license = 'GPL-3.0',
   packages=find_packages(),
   install_requires=[
     'watchdog',
