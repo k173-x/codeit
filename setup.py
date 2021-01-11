ea4e5036a832273a0549839e75c6874274093b43
Aadhitya A <59508546+echo-864@users.noreply.github.com>
2021-01-11 21:02:50 +0530

    Add details
    Signed-off-by: Aadhitya A <59508546+echo-864@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ setup(
   name='codeit',
   version='0.0.3',
   author='Aadhitya A',
+  author_email='aadhitya864@gmail.com',
   description = 'A CLI tool to create CPP files for any contest',
   license = 'GPL-3.0',
+  url="https://github.com/Arch2x/codeit",
   packages=find_packages(),
+  python_requires='>=3.5',
   install_requires=[
     'watchdog',
     'clint'
