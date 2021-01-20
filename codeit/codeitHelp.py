2a6f5415e28a4a2414c6eab418e41d73fc816481
Aadhitya A <59508546+echo-864@users.noreply.github.com>
2021-01-20 16:00:28 +0530

    Revert "Add whitespace in help"
    This reverts commit 581f0dbcb013c14917fa4c5c2e8dd9ee16afbe48.

--- codeit/codeitHelp.py
+++ codeit/codeitHelp.py
@@ -16,7 +16,7 @@ def help():
     print(colored.magenta("codeit <args> <filename/contestname>\n"))
     print(colored.red('Args list\n'))
     print(colored.yellow("-h -------------------------- " + colored.blue("To display help menu")))
-    print(colored.yellow("-i <contest> ---------------- " + colored.blue("To initialize a contest directory")))
+    print(colored.yellow("-i <contest> ----------------" + colored.blue("To initialize a contest directory")))
     print(colored.yellow("-i -b <contest> ------------- " + colored.blue("To initialize a contest directory for practice and beginner level")))
     print(colored.yellow("-i <args> -ne <contest> ----- " + colored.blue("To initialize a contest directory without error.txt file")))
     print(colored.yellow("-i <args> -t2 <contest> ----- " + colored.blue("To initialize a contest directory and use 2nd template without error.txt file")))
