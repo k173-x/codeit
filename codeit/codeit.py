d1768295c0f1a511e2f23373e7d1ca9e180efd79
Sumit Chauhan <cosmictunnel376@gmail.com>
2021-01-22 20:58:08 +0530

    Added 3rd Function to create files only in Current Directory

--- codeit/codeit.py
+++ codeit/codeit.py
@@ -3,7 +3,7 @@ import sys
 import os
 from clint.textui import colored
 from codeit.codeitHelp import help
-from codeit.codeitInit import init, init_noerror, init_noerror_agg
+from codeit.codeitInit import init, init_noerror, init_noerror_agg, create_only_files
 from codeit.codeitMeta import template_agg, template_cp, get_filename, get_fn_beginner
 
 def main():
@@ -12,7 +12,7 @@ def main():
 
     else:
         countArg = 0
-        
+
         for arg in sys.argv:
             countArg+=1
 
@@ -48,16 +48,19 @@ def main():
                     contestName = sys.argv[countArg]
                     fileNames = get_filename()
                     init(contestName, fileNames)
+            elif arg == "-f":
+                contestName = sys.argv[countArg]
+                fileNames = get_filename()
+                create_only_files(contestName, fileNames)
 
             elif arg == "-v":
                 print(colored.magenta('CODEIt v0.0.3 (c)2020 - 2021, alphaX86 under Arch-1'))
 
             elif arg == "-h":
                 help()
-            
+
 
 def init_single_file(filename, template='\n'):
   full_filename = os.path.join(os.getcwd(), filename)
   with open(full_filename, 'w+') as f:
     f.write(template)
-
--- codeit/codeitHelp.py
+++ codeit/codeitHelp.py
@@ -5,10 +5,10 @@ def help():
 
     print(colored.white(""" ██████╗ ██████╗ ██████╗ ███████╗██╗████████╗
 ██╔════╝██╔═══██╗██╔══██╗██╔════╝██║╚══██╔══╝
-██║     ██║   ██║██║  ██║█████╗  ██║   ██║   
-██║     ██║   ██║██║  ██║██╔══╝  ██║   ██║   
-╚██████╗╚██████╔╝██████╔╝███████╗██║   ██║   
- ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝   
+██║     ██║   ██║██║  ██║█████╗  ██║   ██║
+██║     ██║   ██║██║  ██║██╔══╝  ██║   ██║
+╚██████╗╚██████╔╝██████╔╝███████╗██║   ██║
+ ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝
                                              """))
 
     print(colored.cyan("A CLI Script tool to help you initialize CPP files for any contest"))
@@ -17,6 +17,7 @@ def help():
     print(colored.red('Args list\n'))
     print(colored.yellow("-h -------------------------- " + colored.blue("To display help menu")))
     print(colored.yellow("-i <contest> ---------------- " + colored.blue("To initialize a contest directory")))
+    print(colored.yellow("-f <contest> ---------------- " + colored.blue("To create only files in the current directory level")))
     print(colored.yellow("-i -b <contest> ------------- " + colored.blue("To initialize a contest directory for practice and beginner level")))
     print(colored.yellow("-i <args> -ne <contest> ----- " + colored.blue("To initialize a contest directory without error.txt file")))
     print(colored.yellow("-i <args> -t2 <contest> ----- " + colored.blue("To initialize a contest directory and use 2nd template without error.txt file")))
