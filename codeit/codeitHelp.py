d316e1f2b65caaf2614395aa315818fd8806d9bf
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-01-23 23:12:49 +0530

    Updated the colour scheme and args pattern.

--- codeit/codeitHelp.py
+++ codeit/codeitHelp.py
@@ -1,24 +1,31 @@
 from clint.textui import colored
 
+
 def help():
+    function = {"  -h ": " To display help menu.",
+                "  -i <contest> ": " To initialize a contest directory.",
+                "  -i -b <contest> ": " To initialize a contest directory for practice and beginner level.",
+                "  -i <args> -ne <contest> ": " To initialize a contest directory without error.txt file.",
+                "  -i <args> -t2 <contest> ": " To initialize a contest directory and use 2nd template without error.txt file.",
+                "  -i -n <file> ": " To initialize only a single file.",
+                "  -v ": " To print version number."}
+
     print('\n')
 
-    print(colored.white(""" ██████╗ ██████╗ ██████╗ ███████╗██╗████████╗
-██╔════╝██╔═══██╗██╔══██╗██╔════╝██║╚══██╔══╝
-██║     ██║   ██║██║  ██║█████╗  ██║   ██║   
-██║     ██║   ██║██║  ██║██╔══╝  ██║   ██║   
-╚██████╗╚██████╔╝██████╔╝███████╗██║   ██║   
- ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝   
-                                             """))
+    print(colored.red("""  ██████╗  ██████╗  ██████╗  ███████╗ ██╗ ████████╗
+ ██╔════╝ ██╔═══██╗ ██╔══██╗ ██╔════╝ ██║ ╚══██╔══╝
+ ██║      ██║   ██║ ██║  ██║ █████╗   ██║    ██║   
+ ██║      ██║   ██║ ██║  ██║ ██╔══╝   ██║    ██║   
+ ╚██████╗ ╚██████╔╝ ██████╔╝ ███████╗ ██║    ██║   
+  ╚═════╝  ╚═════╝  ╚═════╝  ╚══════╝ ╚═╝    ╚═╝   """))
 
-    print(colored.cyan("A CLI Script tool to help you initialize CPP files for any contest"))
-    print('\nCommands:\n')
-    print(colored.magenta("codeit <args> <filename/contestname>\n"))
-    print(colored.red('Args list\n'))
-    print(colored.yellow("-h -------------------------- " + colored.blue("To display help menu")))
-    print(colored.yellow("-i <contest> ----------------" + colored.blue("To initialize a contest directory")))
-    print(colored.yellow("-i -b <contest> ------------- " + colored.blue("To initialize a contest directory for practice and beginner level")))
-    print(colored.yellow("-i <args> -ne <contest> ----- " + colored.blue("To initialize a contest directory without error.txt file")))
-    print(colored.yellow("-i <args> -t2 <contest> ----- " + colored.blue("To initialize a contest directory and use 2nd template without error.txt file")))
-    print(colored.yellow("-i -n <file> ---------------- " + colored.blue("To initialize only a single file")))
-    print(colored.yellow("-v -------------------------- " + colored.blue("To print version number")))
+    print(colored.cyan(
+        "\n The CLI Script tool to help you initialize CPP files for any contest."))
+
+    print(colored.red('\n Command: '))
+    print(colored.cyan("  codeit <args> <filename/contestname>"))
+
+    print(colored.red('\n Args list: '))
+    for arg, func in function.items():
+        print(colored.red(arg) + '-'*(30-len(arg)) + '>' + colored.cyan(func))
+    print('\n')
