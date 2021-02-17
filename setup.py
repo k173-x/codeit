3fb4742bff6ca776edeb5dfa796c1cf598301806
Aadhitya A <aadhitya864@gmail.com>
2021-02-17 21:35:43 +0530

    Refining pkg to ver 1.0.0
    Signed-off-by: Aadhitya A <aadhitya864@gmail.com>

--- setup.py
+++ setup.py
@@ -2,16 +2,23 @@ from setuptools import setup,find_packages
 
 setup(
   name='codeit',
-  version='0.0.3',
+  packages = find_packages(),
+  version='1.0.0',
   author='Aadhitya A',
   author_email='aadhitya864@gmail.com',
   description = 'A CLI tool to create CPP files for any contest',
   long_description = 'readme',
   long_description_content_type="text/markdown",
   license = 'GPL-3.0',
+  py_modules=['codeit'],
   url="https://github.com/Arch2x/codeit",
-  packages=find_packages(),
+  classifiers=(
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.8'
+    ),
   python_requires='>=3.5',
+  include_package_data = True,
   install_requires=[
     'watchdog',
     'clint'
