7e859e19596393aa8f28f67087f13467a6562438
Aadhitya A <aadhitya864@gmail.com>
2021-02-18 15:16:43 +0530

    Rename package to avoid conflict
    Signed-off-by: Aadhitya A <aadhitya864@gmail.com>

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup,find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-  name='codeit',
+  name='cxit',
   packages = find_packages(),
   version='1.0.0',
   author='Aadhitya A',
   author_email='aadhitya864@gmail.com',
   description = 'A CLI tool to create CPP files for any contest',
-  long_description = 'readme',
+  long_description = long_description,
   long_description_content_type="text/markdown",
   license = 'GPL-3.0',
   py_modules=['codeit'],
